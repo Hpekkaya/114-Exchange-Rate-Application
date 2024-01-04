4c233cab43b7fb16955e4cc8a2c080998ea00fd8
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-04 08:23:19 +0300

    exchange function

--- javascript.js
+++ javascript.js
@@ -1,17 +1,23 @@
 // Lets select elements 
 
-const amountInput =document.querySelector("amount");
-const firstOption =document.querySelector("firstCurrencyOption");
-const amountOption =document.querySelector("secondCurrencyOption");
-const resultInput =document.querySelector("result");
+const amountInput = document.querySelector("#amount");
+const firstOption = document.querySelector("#firstCurrencyOption");
+const secondOption = document.querySelector("#secondCurrencyOption");
+const resultInput = document.querySelector("#result"); 
 
+// const currency = new Currency();
 
 runEventListeners();
 
 function runEventListeners(){
-    amountInput.addEventListener("input", exchange)
+    amountInput.addEventListener("input",exchange);
 }
 
+
 function exchange(){
-    alert("It run")
+  const amount = Number(amountInput.value.trim());
+  const firstOptionValue = firstOption.options[firstOption.selectedIndex].textContent;
+  const secondOptionValue = secondOption.options[secondOption.selectedIndex].textContent;
+  
+  console.log(firstOptionValue, secondOptionValue)
 }
